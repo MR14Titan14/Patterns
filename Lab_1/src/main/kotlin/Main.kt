e7fde0f379c2950942b24a94a372a2261daf00db
MR14Titan14 <sansen184@gmail.com>
2024-09-16 05:40:14 +0300

    1.7 Добавлена проверка корректности ввода полей

--- Lab_1/src/main/kotlin/Main.kt
+++ Lab_1/src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ import Student
 class Main {
     fun main()
     {
-        val petya = Student("Vetkov","Petr","Govnokodov",_phone = "8999a9b9c9+")
-        val vika = Student("Shelestak","Vika","Lepyohovna", _phone = "+79999999999")
+        val petya = Student("Vetkov","Petr","Govnokodov","8999a9b9c9+","asba__XXX","mailexample.com","https://github.com")
+        val vika = Student("Shelestak","Vika","Lepyohovna", "+79999999999","@asba_XXX15","mailexample@gmail.com","https://github.com/Vilieba")
         println(petya.toString())
         println(vika.toString())
     }
