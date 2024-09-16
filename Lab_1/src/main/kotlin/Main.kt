1738a57b714707fc59f69152f06fb9fe858eb3c6
MR14Titan14 <sansen184@gmail.com>
2024-09-16 05:01:48 +0300

    1.6 Добавлен метод класса validatePhone, модифицирован set поля phone

--- Lab_1/src/main/kotlin/Main.kt
+++ Lab_1/src/main/kotlin/Main.kt
@@ -2,10 +2,10 @@ import Student
 class Main {
     fun main()
     {
-        val petya = Student("Vetkov","Petr","Govnokodov")
-        val vika = Student("Shelestak","Vika","Lepyohovna")
-        petya.write()
-        vika.write()
+        val petya = Student("Vetkov","Petr","Govnokodov",_phone = "8999a9b9c9+")
+        val vika = Student("Shelestak","Vika","Lepyohovna", _phone = "+79999999999")
+        println(petya.toString())
+        println(vika.toString())
     }
 }
 
