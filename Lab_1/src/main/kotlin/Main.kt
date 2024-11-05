0b82ab2a3ff5cae5f884f3aeb2caded90ff2e065
MR14Titan14 <sansen184@gmail.com>
2024-11-05 06:17:18 +0300

    3.6 Проведен рефакторинг кода, обновлена диаграмма классов

--- Lab_1/src/main/kotlin/Main.kt
+++ Lab_1/src/main/kotlin/Main.kt
@@ -1,24 +1,25 @@
-import Student
+import Strategy.*
+
 class Main {
     fun main()
     {
-        val petya = Student("Vetkov","Petr","Govnokodov","8999a9b9c9+","asba__XXX","mailexample.com","https://github.com")
-        val vika = Student("Shelestak","Vika","Lepyohovna", "+79999999999","@asba_XXX15","mailexample@gmail.com","https://github.com/Vilieba")
-        println(petya.toString())
-        println(vika.toString())
-        println(vika.validate())
-        vika.setContacts("89999999999","wrongtelegram","mail@yandex.ru")
-        println(vika.toString())
-        var igorInp= hashMapOf<String,Any?>(
-            "lastname" to "Igorko",
-            "name" to "Igor",
-            "fathername" to "Igorevich"
-        )
-        var igor=Student(igorInp)
-        println(igor.toString())
-
-        var danila=Student("Daniil Danilanin Danilavich")
-        println(danila.toString())
+//        val petya = Student("Vetkov","Petr","Govnokodov","8999a9b9c9+","asba__XXX","mailexample.com","https://github.com")
+//        val vika = Student("Shelestak","Vika","Lepyohovna", "+79999999999","@asba_XXX15","mailexample@gmail.com","https://github.com/Vilieba")
+//        println(petya.toString())
+//        println(vika.toString())
+//        println(vika.validate())
+//        vika.setContacts("89999999999","wrongtelegram","mail@yandex.ru")
+//        println(vika.toString())
+//        var igorInp= hashMapOf<String,Any?>(
+//            "lastname" to "Igorko",
+//            "name" to "Igor",
+//            "fathername" to "Igorevich"
+//        )
+//        var igor=Student(igorInp)
+//        println(igor.toString())
+//
+//        var danila=Student("Daniil Danilanin Danilavich")
+//        println(danila.toString())
 //        println(vika.getInfo())
 //        var vik=StudentShort(vika)
 //        println(vik.toString())
@@ -33,24 +34,36 @@ class Main {
 //        println(dtt.getElement(2,2))
 //        println(dtt.getRows())
 //        println(dtt.getColumns())
-        var dlss=DataListStudentShort(mutableListOf(
-            StudentShort(petya),
-            StudentShort(vika),
-            StudentShort(igor),
-            StudentShort(danila)
-        ))
-        dlss.select(1)
-        dlss.select(2)
-        var dtss=dlss.getTable()
-        for (i in 0..dtss.getRows()-1)
-        {
-            for(j in 0..dtss.getColumns()-1)
-            {
-                print(dtss.getElement(i,j))
-                print(" ")
-            }
-            println()
-        }
+//        var dlss=DataListStudentShort(mutableListOf(
+//            StudentShort(petya),
+//            StudentShort(vika),
+//            StudentShort(igor),
+//            StudentShort(danila)
+//        ))
+//        dlss.select(1)
+//        dlss.select(2)
+//        var dtss=dlss.getTable()
+//        for (i in 0..dtss.getRows()-1)
+//        {
+//            for(j in 0..dtss.getColumns()-1)
+//            {
+//                print(dtss.getElement(i,j))
+//                print(" ")
+//            }
+//            println()
+//        }
+    var manager = StudentManager(StudentListTxt())
+    manager.readFromFile("input.txt")
+    println(manager.data)
+    manager.writeToFile("output.txt")
+    manager.setStrategy(StudentListJson())
+    manager.readFromFile("input.json")
+    println(manager.getById(1))
+    manager.writeToFile("output.json")
+    manager.setStrategy(StudentListYaml())
+    manager.readFromFile("input.yaml")
+    println(manager.getById(1))
+    manager.writeToFile("output.yaml")
     }
 }
 
